b165df7428f881f66fd7d42a3e79bf9186b146c7
jk2pr <jk2praj@gmail.com>
2020-02-20 16:50:22 +0800

    Created Appbar using Jetpack Compose

--- app/src/main/java/com/jk/ml/composer/MainActivity.kt
+++ app/src/main/java/com/jk/ml/composer/MainActivity.kt
@@ -6,19 +6,16 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.compose.Composable
+import androidx.compose.unaryPlus
+import androidx.ui.core.Alignment
 import androidx.ui.core.Text
 import androidx.ui.core.dp
 import androidx.ui.core.setContent
 import androidx.ui.foundation.VerticalScroller
 import androidx.ui.graphics.Color
-import androidx.ui.layout.Column
-import androidx.ui.layout.CrossAxisAlignment
-import androidx.ui.layout.FlexRow
-import androidx.ui.layout.Padding
-import androidx.ui.material.Button
-import androidx.ui.material.ContainedButtonStyle
-import androidx.ui.material.Divider
-import androidx.ui.material.MaterialTheme
+import androidx.ui.layout.*
+import androidx.ui.material.*
+import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
 
 
@@ -36,20 +33,30 @@ class MainActivity : AppCompatActivity() {
     @Composable
     fun Test() {
 
-
         MaterialTheme {
-            VerticalScroller {
-                Column {
-                    (0..50).forEachIndexed { index, _ ->
-                        createListItem(index)
-                        Divider(color = Color.Blue, height = 1.dp)
+
+            Column {
+                TopAppBar(
+                    title = {
+                        Text(text = "Composer")
+                    },
+                    color = Color.White
+
+                )
+                VerticalScroller {
+                    Column {
+                        (0..100).forEachIndexed { index, _ ->
+                            createListItem(index)
+                            Divider(color = Color.Blue, height = 1.dp)
+                        }
                     }
+
                 }
             }
-
         }
-
     }
+
+
     @Composable
     private fun createListItem(itemIndex: Int) {
         Padding(left = 8.dp, right = 8.dp, top = 8.dp, bottom = 8.dp) {
@@ -72,19 +79,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
+
 }
